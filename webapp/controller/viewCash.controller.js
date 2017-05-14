59ffe310a48b4d1cccf972fd276d91494307c8ec
Debanjana Sarkar <dsarkar0411@gmail.com>
2017-05-14 17:59:51 +0000

    controller error solve

--- webapp/controller/viewCash.controller.js
+++ webapp/controller/viewCash.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
-
+/*eslint linebreak-style: ["error", "windows"]*/
 	return Controller.extend("com.demoTMS.controller.viewCash", {
 
 		/**
