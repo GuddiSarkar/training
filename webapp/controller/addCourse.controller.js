1ed716ef932f14fa864a506d2b8971fc258e02ce
Ravikant Ranjan <rkranjan2605@gmail.com>
2017-05-15 18:49:09 +0000

    save course

--- webapp/controller/addCourse.controller.js
+++ webapp/controller/addCourse.controller.js
@@ -7,6 +7,7 @@ sap.ui.define([
 /*eslint linebreak-style: ["error", "windows"]*/
 	return Controller.extend("com.demoTMS.controller.addCourse", {
 
+<<<<<<< Upstream, based on fe3cd97aa492a2eb3e88c6ac1cdd49f66c761fb3
 			onSearch: function(oEvent){
 		    var oTable = this.getView().byId("table");
 			var oBinding = oTable.getBinding("rows");
@@ -18,6 +19,49 @@ sap.ui.define([
 			var allFilter = new Filter([oFilter1, oFilter2,oFilter3,oFilter4], false); 
 			oBinding.filter(allFilter);
 		}
+=======
+		addCourse: function(oEvent)
+		{
+			var courseName = this.getView().byId("c_name").getValue();
+			var courseFee = this.getView().byId("c_fee").getValue();
+			var courseType = this.getView().byId("c_type").getValue();
+			var courseDuration = this.getView().byId("c_dur").getSelectedKey();
+			var oModel = this.getOwnerComponent().getModel();
+			oModel.setUseBatch(false);
+			var oCatDef = $.Deferred();
+			oModel.read("/tb_course", 
+			{
+				success: function(oData) 
+				{
+					this.nextId = oData.results.length;
+					oCatDef.resolve();
+				}.bind(this),
+				error: function(oErr) 
+				{
+					this.nextId = null;
+					oCatDef.reject();
+				}.bind(this)
+			});
+
+			$.when(oCatDef).then(
+				function() 
+				{
+					var oEntry = 
+					{
+						"course_id": this.nextId,
+						"course_name": courseName,
+						"course_type": courseType,
+						"course_fee": courseFee,
+						"course_duration": courseDuration
+					};
+					oModel.create("/tb_course", oEntry);
+
+				}.bind(this)
+			);
+			oModel.setRefreshAfterChange(true);
+		}
+
+>>>>>>> 1f70dfc save course
 	});
 
 });
