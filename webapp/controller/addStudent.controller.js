84a9b2f220be7fc4abfb917132d758bfa4e3f2c1
Ravikant Ranjan <rkranjan2605@gmail.com>
2017-05-21 11:03:22 +0000

    add stud

--- webapp/controller/addStudent.controller.js
+++ webapp/controller/addStudent.controller.js
@@ -10,18 +10,48 @@ sap.ui.define([
 			oModel.setUseBatch(false);
 		},
 
+		onSelectCourse: function(oEvent){
+		var crs_id = this.getView().byId("c_name").getSelectedKey();
+		var oModel = this.getOwnerComponent().getModel("course");
+		this.getView().setModel(oModel);
+		oModel.read("/tb_course('" + crs_id + "')",{
+					success: function(oData, oResponse) {
+						this.getView().byId("c_fee").setValue(oData.course_fee);
+						this.getView().byId("c_type").setValue(oData.course_type);
+						this.getView().byId("c_fee1").setValue(oData.course_fee);
+				}.bind(this),
+				error: function(error)
+				{
+					
+				}.bind(this)
+				});
+	},
+	
+	onChangeInput: function(oEvent){
+		var crs_fee = this.getView().byId("c_fee1").getValue();
+		var discount = this.getView().byId("d_scunt").getValue();
+		var net_fee = parseInt(crs_fee-((crs_fee*discount)/100));
+		this.getView().byId("r_fee").setValue(1000);
+		this.getView().byId("tp_amnt").setValue(net_fee);
+		var instlmnt = parseInt((net_fee-1000)/3);
+		this.getView().byId("f_instl").setValue(instlmnt);
+		this.getView().byId("sec_instl").setValue(instlmnt);
+		this.getView().byId("thrd_instl").setValue(instlmnt);
+		
+	},
+
 	addStudent:function(oEvent)
 	{
 			var firstName = this.getView().byId("f_name").getValue();
 			var lastName = this.getView().byId("l_name").getValue();
 			var name = firstName+" "+lastName;
-			var gender = this.getView().byId("g_nder").getValue();
+			var gender = this.getView().byId("g_nder").getSelectedButton().getText();
 			var dob = this.getView().byId("d_ob").getValue();
 			var phone = this.getView().byId("p_hone").getValue();
 			var email = this.getView().byId("e_ml").getValue();
 			var addstreet1 = this.getView().byId("a_strt1").getValue();
 			var addstreet2 = this.getView().byId("a_strt2").getValue();
-			var address = addstreet1+""+addstreet2;
+			var address = addstreet1+" "+addstreet2;
 			var city = this.getView().byId("c_ty").getValue();
 			var state = this.getView().byId("s_tate").getValue();
 			var zip = this.getView().byId("zip_code").getValue();
@@ -39,23 +69,8 @@ sap.ui.define([
 				"stud_gender": gender, 
 				"stud_dob": dob, 
 				"stud_mob": phone,
-				"stud_email":email
-			};
-			var oEntryAdd = {
-				"address_street":address,
-				"address_city":city,
-				"adress_state":state,
-				"adress_zip":zip
-			};
-			var oEntryStudPay = {
-				"course_id":crs_id,
-				"stud_payment_disc":discount,
-				"stud_payment_tax":tax,
-				"stud_payment_fee":totalPaybleAmount,
-				"stud_payment_reg_fee":registrationFee,
-				"stud_payment_instal_1":fstInstlment,
-				"stud_payment_instal_2":secondInstlment,
-				"stud_payment_instal_3":thrdInstlment
+				"stud_email":email,
+				"stud_strt_date":startDate
 			};
 			var oModelStud = this.getOwnerComponent().getModel("student");
 			oModelStud.setUseBatch(false);
@@ -73,6 +88,13 @@ sap.ui.define([
 			);
 			oModelStud.setRefreshAfterChange(true);
 			
+			
+			var oEntryAdd = {
+				"address_street":address,
+				"address_city":city,
+				"adress_state":state,
+				"adress_zip":zip
+			};
 			var oModelAdd = this.getOwnerComponent().getModel("address");
 			oModelAdd.setUseBatch(false);
 			oModelAdd.create("/tb_address",oEntryAdd,
@@ -89,6 +111,17 @@ sap.ui.define([
 			);
 			oModelAdd.setRefreshAfterChange(true);
 			
+			
+			var oEntryStudPay = {
+				"course_id":crs_id,
+				"stud_payment_disc":discount,
+				"stud_payment_tax":tax,
+				"stud_payment_fee":totalPaybleAmount,
+				"stud_payment_reg_fee":registrationFee,
+				"stud_payment_instal_1":fstInstlment,
+				"stud_payment_instal_2":secondInstlment,
+				"stud_payment_instal_3":thrdInstlment
+			};
 			var oModelStudPay = this.getOwnerComponent().getModel("address");
 			oModelStudPay.setUseBatch(false);
 			oModelStudPay.create("/tb_stud_payment",oEntryStudPay,
@@ -105,28 +138,7 @@ sap.ui.define([
 			);
 			oModelStudPay.setRefreshAfterChange(true);
 			
-		
-				var filterid= new Filter("crs_id",FilterOperator.EQ,course_id);
-				var oFilter=new Filter({
-					filters: [filterid]
-				});
-				oModel.read("/tb_course",{
-					filters: [oAllFilters],
-					success: function(oData, oResponse) {
-						var cModel = new sap.ui.model.json.JSONModel();
-						sap.ui.getCore().setModel(cModel,"crsModel");
-						cModel.setData(oData.results);
-				}
-				});
-			
-	},
-	
-	onSelectionchange: function(oEvent){
-		var oData = sap.ui.getCore().getModel("crsModel").getData();
-		this.getView().byId("c_fee").setText(oData.course_fee);
-		this.getView().byId("c_type").setText(oData.course_type);
 	}
-
 	});
 
 });
