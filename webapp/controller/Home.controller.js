31c8bc70ebc490f6f55e289432af3afd4472ab6e
Ravikant Ranjan <rkranjan2605@gmail.com>
2017-06-12 08:55:37 +0000

    login page

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,11 +1,21 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	'jquery.sap.global',
+	'sap/ui/model/SimpleType',
+	'sap/ui/model/ValidateException',
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
 
 ], function(Controller) {
 	"use strict";
 /*eslint linebreak-style: ["error", "windows"]*/
 	return Controller.extend("com.demoTMS.controller.Home", {
-
-
+		getRouter: function() {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		
+		onLogin: function(evt) {
+			this.getRouter().navTo("login");
+		}
 	});
 });
