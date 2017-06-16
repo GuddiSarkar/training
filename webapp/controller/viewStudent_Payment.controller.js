7bd57146ba107c67ea9f22f1d3a1707f56e68bf7
Debanjana Sarkar <dsarkar0411@gmail.com>
2017-06-16 12:18:40 +0000

    by debanjana

--- webapp/controller/viewStudent_Payment.controller.js
+++ webapp/controller/viewStudent_Payment.controller.js
@@ -3,12 +3,12 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("com.demoTMS.controller.view_by_title", {
+	return Controller.extend("com.demoTMS.controller.viewStudent_Payment", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.demoTMS.view.view_by_title
+		 * @memberOf com.demoTMS.view.viewStudent_Payment
 		 */
 		//	onInit: function() {
 		//
@@ -17,7 +17,7 @@ sap.ui.define([
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.demoTMS.view.view_by_title
+		 * @memberOf com.demoTMS.view.viewStudent_Payment
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -26,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.demoTMS.view.view_by_title
+		 * @memberOf com.demoTMS.view.viewStudent_Payment
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -34,7 +34,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.demoTMS.view.view_by_title
+		 * @memberOf com.demoTMS.view.viewStudent_Payment
 		 */
 		//	onExit: function() {
 		//
