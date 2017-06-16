d34a764762b9ce451b38f0a14cc0254783ad4b55
Debanjana Sarkar <dsarkar0411@gmail.com>
2017-06-16 20:23:35 +0000

    Admin

--- webapp/controller/Approve.controller.js
+++ webapp/controller/Approve.controller.js
@@ -2,43 +2,37 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
-
+	/*eslint linebreak-style: ["error", "windows"]*/
 	return Controller.extend("com.demoTMS.controller.Approve", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.demoTMS.view.Approve
-		 */
-		//	onInit: function() {
-		//
-		//	},
+		addSave: function(oEvent) {
+			var Name = this.getView().byId("a_name").getValue();
+			var Role = this.getView().byId("a_role").getValue();
+			var SetUserName = this.getView().byId("a_uname").getValue();
+			var SetPassword = this.getView().byId("a_pwd").getValue();
+			var oEntry = {
+				"user_username": SetUserName,
+				"user_pwd": SetPassword,
+				"user_role": Role,
+				"user_name": Name
+			};
+			var oModel = this.getOwnerComponent().getModel("course");
+			oModel.setUseBatch(false);
+			oModel.create("/tb_user", oEntry, {
+				success: function(oData) {
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.demoTMS.view.Approve
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+					this.getView().byId("a_uname").setValue("");
+					this.getView().byId("a_pwd").setValue("");
+					this.getView().byId("a_role").setValue("");
+					this.getView().byId("a_name").setValue("");
+				}.bind(this),
+				error: function(error) {
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.demoTMS.view.Approve
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+				}.bind(this)
+			});
 
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.demoTMS.view.Approve
-		 */
-		//	onExit: function() {
-		//
-		//	}
+			oModel.setRefreshAfterChange(true);
+		}
 
 	});
 
