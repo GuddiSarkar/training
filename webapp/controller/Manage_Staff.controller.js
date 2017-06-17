7cd79ece8b43cecc3b3b20d36f175fb6bb0945df
Debanjana Sarkar <dsarkar0411@gmail.com>
2017-06-17 06:11:42 +0000

    chng by debanjana

--- webapp/controller/Manage_Staff.controller.js
+++ webapp/controller/Manage_Staff.controller.js
@@ -1,44 +1,47 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+], function(Controller, Filter, FilterOperator) {
 	"use strict";
-
+	/*eslint linebreak-style: ["error", "windows"]*/
 	return Controller.extend("com.demoTMS.controller.Manage_Staff", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.demoTMS.view.Manage_Staff
-		 */
-		//	onInit: function() {
-		//
-		//	},
+	onInit: function() {
+			var oModel = this.getOwnerComponent().getModel("course");
+			oModel.setUseBatch(false);
+	},
+	
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.demoTMS.view.Manage_Staff
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+	onClickDelete: function(oEvent) {
+			var oView = this.getView();
+			var oTable = this.getView().byId("Table");
+			var path = oEvent.getSource().getBindingContext("course").getPath();
+			var model = oTable.getModel("course");
+			var property = model.getProperty(path);
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.demoTMS.view.Manage_Staff
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.demoTMS.view.Manage_Staff
-		 */
-		//	onExit: function() {
-		//
-		//	}
+			var oModel = this.getOwnerComponent().getModel("course");
+			oModel.remove(path, {
+				success: function(oData, oResponse) {
+					console.log(oData);
+					console.log(oResponse);
+				}.bind(this),
+				error: function(err) {
+					console.log(err);
+				}.bind(this)
+			});
+			oModel.setRefreshAfterChange(true);
+		},
+		onSearch: function(oEvent) {
+			var oTable = this.getView().byId("Table");
+			var oBinding = oTable.getBinding("items");
+			var value = oEvent.getParameter("query");
+			var oFilter1 = new Filter("user_name", FilterOperator.Contains, value);
+			var oFilter2 = new Filter("user_username", FilterOperator.Contains, value);
+			var oFilter3 = new Filter("user_role", FilterOperator.Contains, value);
+			var allFilter = new Filter([oFilter1, oFilter2, oFilter3], false);
+			oBinding.filter(allFilter);
+		}
 
 	});
 
